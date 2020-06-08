8dcc0ce1d5b053a49bb40b950044cafd27298bb7
iamsosassy <shirleyrivera70@gmail.com>
2020-06-08 14:39:21 -0400

    inital pages for portfolio profile

---
+++ src/Component/About/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+export default class About extends Component {
+    render() {
+        Let resumeData = this.props.resumeData;
+        return (
+            <section id="about">
+                <div className="row">
+                    <div className="three colums">
+
+                    </div>
+
+                </div>
+
+            </section>
+        )
+
+    }
+}
