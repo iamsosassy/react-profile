6dad55a027c16e4bd7b5d2ccc09b9f85470f8964
iamsosassy <shirleyrivera70@gmail.com>
2020-06-17 09:57:45 -0400

    debugged issues with webpack

---
+++ src/resumeData.js
@@ -0,0 +1,77 @@
+let resumeData = {
+  "imagebaseurl": "https://iamsosassy.github.io/",
+  "name": "Shirley Rivera",
+  "role": "Full-Stack Software Engineer",
+  "linkedinId": "https://www.linkedin.com/in/shirleybrivera/",
+  "githubID": "https://github.com/iamsosassy",
+  "roleDescription": "Full-Stack Software Engineer recent graduated from Rutgers Coding Bootcamp. Enjoy learning about new technologies, building and creating web applications. ",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/",
+      "className": "fa fa-github"
+    },
+
+  ],
+  "aboutme": "Full Stack (MERN) Developer with Management and Leadership Experience in Healthcare",
+  "address": "Jersey City, New Jersey",
+  "education": [
+    {
+      "UniversityName": "Rutgers University",
+      "MonthOfPassing": "June",
+      "YearOfPassing": "2020",
+      "Achievements": "Certification for Coding Bootcamp"
+    }
+  ],
+
+  "skillsDescription": "Front-End and Back-End ",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "NodeJS"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "MySQL"
+    },
+    {
+      "skillname": "Sequelize"
+    },
+    {
+      "skillname": "Express"
+    }
+    // ],
+    // "portfolio": [
+    //   {
+    //     "name": "project3",
+    //     "description": "bookstoshare",
+    //     "url": "https://glacial-bastion-95752.herokuapp.com/"
+    //   },
+
+  ],
+
+
+}
+
+export default resumeData
